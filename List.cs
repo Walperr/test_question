ff5ab2fd85e76a7578c1b0b8ecc99503ed6a4ab8
Walperr <Vladbann37@yandex.ru>
2021-03-20 00:38:10 +0700

    Initial commit

---
+++ List.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace test_question
+{
+
+    class List<T> : IEnumerable<T>
+    {
+        private Node<T> Head { get; set; }
+
+        public List()
+        {
+            Head = null;
+        }
+
+        public List(T data)
+        {
+            Head = new Node<T>(data);
+        }
+
+        public void Add(T data)
+        {
+            if (Head == null)
+            {
+                Head = new Node<T>(data);
+                return;
+            }
+
+            var temp = new Node<T>(Head.data);
+
+            temp.next = Head.next;
+
+            Head.next = temp;
+
+            Head.data = data;
+        }
+
+        public void Remove()
+        {
+            if (Head == null) return;
+
+            if (Head.next != null)
+            {
+                Head.data = Head.next.data;
+                Head.next = Head.next.next;
+            }
+            else
+                Head = null;
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            return new ListEnumerator<T>(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new ListEnumerator<T>(this);
+        }
+
+        class Node<T1>
+        {
+            public T1 data;
+
+            public Node<T1> next;
+
+            public Node(T1 data)
+            {
+                this.data = data;
+                next = null;
+            }
+        }
+        class ListEnumerator<T2> : IEnumerator<T2>
+        {
+            private List<T2> list;
+
+            T2 IEnumerator<T2>.Current { get { return current.data; } }
+
+            object IEnumerator.Current { get { return current.data; } }
+
+            private List<T2>.Node<T2> current;
+
+            public ListEnumerator(List<T2> list)
+            {
+                this.list = list;
+                current = null;
+            }
+
+            public bool MoveNext()
+            {
+                if (list.Head == null)
+                    return false;
+
+                if (current == null)
+                {
+                    current = list.Head;
+                    return true;
+                }
+                if (current.next != null)
+                {
+                    current = current.next;
+                    return true;
+                }
+                else return false;
+            }
+            public void Reset()
+            {
+                current = list.Head;
+            }
+            void IDisposable.Dispose()
+            {
+                Reset();
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace test_question
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+
+            list.Add("one");
+            list.Add("three");
+            list.Add("four");
+            list.Add("seven");
+            list.Add("zero");
+
+            foreach (var i in list)
+                Console.WriteLine(i);
+
+            list.Remove();
+            list.Remove();
+
+            Console.WriteLine("-------------");
+
+            foreach (var i in list)
+                Console.WriteLine(i);
+        }
+    }
+}
